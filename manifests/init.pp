fb9b5bdcc34267c21400548aac7bd1a2a810dd2f
Ryan Kois <ryan.kois@gmail.com>
2013-05-12 09:00:41 -0600

    More vim stuff.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,16 @@
-class vim($home = '/home/vagrant') {
+class vim() {
   class{'vim::install': } ->
-  class{'vim::config': } ->
+  class{'vim::cleanup': } ->
+  class{'vim::plugins': } ->
   Class['vim']
 }
 
 class vim::install {
-  Package { ensure => installed }
+  $home = '/home/vagrant'
+
   $vimPackages = [
     'gcc',
     'make',
-    'make-install',
     'ncurses',
     'ncurses-devel',
     'lua',
@@ -21,23 +22,58 @@ class vim::install {
     'perl',
     'wget',
     ]
-  package { $vimPackages: } ->
-  exec {"wget -o $home/vagrant/vim-7.3.tar.bz2 ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2": 
-    creates =>  '/usr/local/bin/vim'
+  package { $vimPackages: ensure => installed} ->
+  exec {"wget -q ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2":
+    creates =>  '/usr/local/bin/vim',
   } ->
-  exec {"tar xvjf $home/vim-7.3.tar.bz2": 
-    creates =>  '/usr/local/bin/vim'
+  file {'/home/vagrant/vim-7.3.tar.bz2':
+    owner => 'root',
+    group => 'root',
+		mode => '0664',
+	} ->
+  exec {"tar xvjf ${home}/vim-7.3.tar.bz2":
+    creates =>  '/usr/local/bin/vim',
   } ->
-  exec {"${home}/vim73/configure --disable-selinux --enable-luainterp \
-  --enable-perlinterp \
-  --enable-pythoninterp \
-  --enable-python3interp \
-  --enable-tclinterp \
-  --enable-rubyinterp \
-  --enable-multibyte \
-  --disable-gui \
-  --with-features=huge":
-    creates =>  '/usr/local/bin/vim'
-    } ->
-  exec {"${home}/vim73/make && ${home}/vim73/make install":}
+	file {'/home/vagrant/vim73':
+    owner => 'root',
+    group => 'root',
+    recurse => true,
+  } ->
+  exec {"cd /home/vagrant/vim73 && ./configure --disable-selinux --enable-luainterp --enable-perlinterp --enable-pythoninterp --enable-tclinterp --enable-rubyinterp --enable-multibyte --disable-gui --with-features=huge && make && make install":
+    provider => 'shell',
+    creates => '/usr/local/bin/vim',
+	}
+}
+
+class vim::cleanup {
+  exec {'rm -rf /home/vagrant/vim73 ; rm -rf /home/vagrant/vim-7.3.tar.bz2':
+		onlyif => "test -f /home/vagrant/vim73"
+  }
+}
+
+class vim::plugins {
+	file {'/home/vagrant/.vim/autoload':
+    ensure => directory,
+    owner => 'vagrant',
+    group => 'vagrant',
+    mode => 0664,
+	}
+  file {'/home/vagrant/.vim/bundle':
+		ensure => directory,
+		owner => 'vagrant',
+		group => 'vagrant',
+    mode => 0664,
+  }
+  exec { "/usr/bin/curl 'www.vim.org/scripts/download_script.php?src_id=19375' > /home/vagrant/.vim/autoload/pathogen.vim":
+    creates => '/home/vagrant/.vim/autoload/pathogen.vim',
+	  require => File['/home/vagrant/.vim/autoload']
+  }
+  file {'/home/vagrant/.vim':
+    owner => 'vagrant',
+    group => 'vagrant',
+    recurse => true,
+  }
+  exec {'git clone https://github.com/tpope/vim-surround.git /home/vagrant/.vim/bundle/vim-surround':
+    creates => '/home/vagrant/.vim/bundle/vim-surround',
+	}
 }
